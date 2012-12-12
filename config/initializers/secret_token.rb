60a079b717c2d2f14827eee812f5e6484cf838b3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-12-12 16:37:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TakeOnRules::Application.config.secret_token = '0fb9a12847cd927976642777535d8247a8bd9cd9c96f88e83448afbbd140ad1bc09081fb58f58a396c70dd5715c9ce0a46626b45cee689b573318b263b702749'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TakeOnRules::Application.config.session_store :cookie_store, key: '_take_on_rules_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TakeOnRules::Application.config.session_store :active_record_store
